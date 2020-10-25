925f0cef67bc041ef957e5e6d76166e98f937a00
Amanska <56720795+Amanska@users.noreply.github.com>
2020-10-25 15:18:41 +0100

    Add files via upload

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,25 +1,26 @@
- const testimonials = document.querySelector('.testimonials');
-   const scroller = testimonials.querySelector('.scroller');
-   const nextBtn = testimonials.querySelector('.btn.next');
-   const prevBtn = testimonials.querySelector('.btn.prev');
-   const itemWidth = testimonials.querySelector('.item').clientWidth;
+const testimonials2 = document.querySelector('.testimonials2');
+const scroller2 = testimonials2.querySelector('.scroller2');
+const nextBtn2 = testimonials2.querySelector('.btn.next2');
+const prevBtn2 = testimonials2.querySelector('.btn.prev2');
+const itemWidth2 = testimonials2.querySelector('.item2').clientWidth;
   
-   nextBtn.addEventListener('mousehover', scrollToNextItem);
-   prevBtn.addEventListener('click', scrollToPrevItem);
+nextBtn2.addEventListener('click', scrollToNextItem2);
+prevBtn2.addEventListener('click', scrollToPrevItem2);
   
-function scrollToNextItem() {
-   if(scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
+function scrollToNextItem2() {
+   if(scroller2.scrollLeft < (scroller2.scrollWidth - itemWidth))
        // The scroll position is not at the beginning of last item
-       scroller.scrollBy({left: itemWidth, top: 0, behavior:'smooth'});
+       scroller2.scrollBy({left: itemWidth2, top: 0, behavior:'smooth'});
    else
        // Last item reached. Go back to first item by setting scroll position to 0
-       scroller.scrollTo({left: 0, top: 0, behavior:'smooth'});
+       scroller2.scrollTo({left: 0, top: 0, behavior:'smooth'});
 }
-function scrollToPrevItem() {
-   if(scroller.scrollLeft != 0)
+function scrollToPrevItem2() {
+   if(scroller2.scrollLeft != 0)
        // The scroll position is not at the beginning of first item
-       scroller.scrollBy({left: -itemWidth, top: 0, behavior:'smooth'});
+       scroller2.scrollBy({left: -itemWidth, top: 0, behavior:'smooth'});
    else
        // This is the first item. Go to last item by setting scroll position to scroller width
-       scroller.scrollTo({left: scroller.scrollWidth, top: 0, behavior:'smooth'});
+       scroller2.scrollTo({left: scroller.scrollWidth, top: 0, behavior:'smooth'});
 }
+
